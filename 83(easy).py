cf421079e57400139f71607649415ce167240966
Wata-Naoki <a8120227@gsuite.si.aoyama.ac.jp>
2023-01-15 14:25:59 +0900

    88（easy）/アルゴリズムの勉強/2023/01/15

--- 83(easy).py
+++ 83(easy).py
@@ -1,6 +1,29 @@
 
 
 # リストの整数値を入力
-head = list(map(int, input().split()))
+# head = list(map(int, input().split()))
 # 重複を削除して出力
-print(list(set(head)))
+# print(list(set(head)))
+
+
+def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if head == None:
+        return None
+    if head.next == None:
+        return head
+        prev = head
+        curr = head.next
+    while curr != None:
+        if curr.val == prev.val:
+            prev.next = curr.next
+            curr = curr.next
+        else:
+            prev = curr
+            curr = curr.next
+
+    return head
+
+
+head = list(map(int, input().split()))
+
+print(deleteDuplicates(head=head))
--- 88(easy).py
+++ 88(easy).py
@@ -10,6 +10,6 @@ num1 += num2
 # print(num1)
 
 num1 = [x for x in num1 if x != 0]
-(num1.sort())
-print(num1)
+num1.sort()
 
+print(num1)
